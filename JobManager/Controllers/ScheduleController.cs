7e4ee0e7eb99f89b850a406eca25ab7bd2cea379
Andrew Lackenby <crimdon@hotmail.com>
2016-11-29 10:25:19 +0000

    updated controller

--- JobManager/Controllers/ScheduleController.cs
+++ JobManager/Controllers/ScheduleController.cs
@@ -15,7 +15,7 @@ namespace JobManager.Controllers
         public ActionResult Index(string dbServer = null)
         {
             List<JobSummary> joblist = new List<JobSummary>();
-            joblist = getJobs();
+            joblist = getJobs(dbServer);
             return View(joblist);
         }
 
@@ -23,105 +23,108 @@ namespace JobManager.Controllers
         private string lastOutcome;
         private DateTime lastRun;
         private DateTime nextRun;
-        private List<JobSummary> getJobs()
+        private List<JobSummary> getJobs(string selectedServer)
         {
             Servers servers = new Servers();
 
             List<JobSummary> joblist = new List<JobSummary>();
-            
+
 
             foreach (string server in servers.Serverlist)
             {
-                ConnectSqlServer connection = new ConnectSqlServer();
-                Server dbServer = connection.Connect(server);
-                DataSet ds = dbServer.ConnectionContext.ExecuteWithResults("exec msdb.dbo.sp_help_job");
+                if (string.IsNullOrEmpty(selectedServer) || selectedServer == server)
+                {
+                    ConnectSqlServer connection = new ConnectSqlServer();
+                    Server dbServer = connection.Connect(server);
+                    DataSet ds = dbServer.ConnectionContext.ExecuteWithResults("exec msdb.dbo.sp_help_job");
 
-                foreach (DataRow row in ds.Tables[0].Rows)
-                {   
-                    switch (row["current_execution_status"].ToString())
+                    foreach (DataRow row in ds.Tables[0].Rows)
                     {
-                        case "1":
-                            jobStatus = "Executing:" + row["current_execution_step"];                          
-                            break;
-                        case "2":
-                            jobStatus = "Waiting for Thread";
-                            break;
-                        case "3":
-                            jobStatus = "Between Retries";
-                            break;
-                        case "4":
-                            jobStatus = "Idle";
-                            break;
-                        case "5":
-                            jobStatus = "Suspended";
-                            break;
-                        case "6":
-                            jobStatus = "[Obsolete]";
-                            break;
-                        case "7":
-                            jobStatus = "PerformingCompletionActions";
-                            break;
-                        default:
-                            jobStatus = "";
-                            break;
+                        switch (row["current_execution_status"].ToString())
+                        {
+                            case "1":
+                                jobStatus = "Executing:" + row["current_execution_step"];
+                                break;
+                            case "2":
+                                jobStatus = "Waiting for Thread";
+                                break;
+                            case "3":
+                                jobStatus = "Between Retries";
+                                break;
+                            case "4":
+                                jobStatus = "Idle";
+                                break;
+                            case "5":
+                                jobStatus = "Suspended";
+                                break;
+                            case "6":
+                                jobStatus = "[Obsolete]";
+                                break;
+                            case "7":
+                                jobStatus = "PerformingCompletionActions";
+                                break;
+                            default:
+                                jobStatus = "";
+                                break;
 
+                        }
+
+                        switch (row["last_run_outcome"].ToString())
+                        {
+                            case "0":
+                                lastOutcome = "Failed";
+                                break;
+                            case "1":
+                                lastOutcome = "Succeeded";
+                                break;
+                            case "2":
+                                lastOutcome = "Retry";
+                                break;
+                            case "3":
+                                lastOutcome = "Canceled";
+                                break;
+                            default:
+                                lastOutcome = "Unknown";
+                                break;
+                        }
+
+                        switch (row["last_run_date"].ToString())
+                        {
+                            case "0":
+                                lastRun = DateTime.MinValue;
+                                break;
+                            default:
+                                lastRun = DateTime.ParseExact(row["last_run_date"].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture)
+                                    .Add(TimeSpan.ParseExact(row["last_run_time"].ToString().PadLeft(6, '0'), "hhmmss", CultureInfo.InvariantCulture));
+                                break;
+                        }
+
+                        switch (row["next_run_date"].ToString())
+                        {
+                            case "0":
+                                nextRun = DateTime.MinValue;
+                                break;
+                            default:
+                                nextRun = DateTime.ParseExact(row["next_run_date"].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture)
+                                    .Add(TimeSpan.ParseExact(row["next_run_time"].ToString().PadLeft(6, '0'), "hhmmss", CultureInfo.InvariantCulture));
+                                break;
+                        }
+
+                        joblist.Add(new JobSummary
+                        {
+                            ServerName = row["originating_server"].ToString(),
+                            JobName = row["name"].ToString(),
+                            Enabled = Convert.ToBoolean(row["enabled"]),
+                            Status = jobStatus,
+                            LastOutcome = lastOutcome,
+                            LastRun = lastRun,
+                            NextRun = nextRun,
+                            Category = row["category"].ToString(),
+                            Scheduled = Convert.ToBoolean(row["has_schedule"])
+                        });
                     }
 
-                    switch (row["last_run_outcome"].ToString())
-                    {
-                        case "0":
-                            lastOutcome = "Failed";
-                            break;
-                        case "1":
-                            lastOutcome = "Succeeded";
-                            break;
-                        case "2":
-                            lastOutcome = "Retry";
-                            break;
-                        case "3":
-                            lastOutcome = "Canceled";
-                            break;
-                        default:
-                            lastOutcome = "Unknown";
-                            break;
-                    }
-
-                    switch (row["last_run_date"].ToString())
-                    {
-                        case "0":
-                            lastRun = DateTime.MinValue;
-                            break;
-                        default:
-                            lastRun = DateTime.ParseExact(row["last_run_date"].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture)
-                                .Add(TimeSpan.ParseExact(row["last_run_time"].ToString().PadLeft(6, '0'), "hhmmss", CultureInfo.InvariantCulture));
-                            break;
-                    }
-
-                    switch (row["next_run_date"].ToString())
-                    {
-                        case "0":
-                            nextRun = DateTime.MinValue;
-                            break;
-                        default:
-                            nextRun = DateTime.ParseExact(row["next_run_date"].ToString(), "yyyyMMdd", CultureInfo.InvariantCulture)
-                                .Add(TimeSpan.ParseExact(row["next_run_time"].ToString().PadLeft(6, '0'), "hhmmss", CultureInfo.InvariantCulture));
-                            break;
-                    }
-
-                    joblist.Add(new JobSummary
-                    {
-                        ServerName = row["originating_server"].ToString(),
-                        JobName = row["name"].ToString(),
-                        Enabled = Convert.ToBoolean(row["enabled"]),
-                        Status = jobStatus,
-                        LastOutcome = lastOutcome,
-                        LastRun = lastRun,
-                        NextRun = nextRun,
-                        Category = row["category"].ToString(),
-                        Scheduled = Convert.ToBoolean(row["has_schedule"])
-                    });
                 }
-                
             }
             return joblist;
         }
