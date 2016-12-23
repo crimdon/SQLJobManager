bb6db3563b2b9f3ca0ebb01e5a2970003e5e228a
crimdon <crimdon@hotmail.com>
2016-12-23 11:51:48 +0000

    Improved validation on schedule model and added confirm delete

--- JobManager/Models/ScheduleDetailsModel.cs
+++ JobManager/Models/ScheduleDetailsModel.cs
@@ -11,30 +11,41 @@ namespace JobManager.Models
     {
         // Applies to all Schedules
         [Required]
+        [Display(Name = "Job name")]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Schedule type")]
         public string ScheduleFrequency { get; set; }
+        [Display(Name = "Enabled")]
         public bool IsEnabled { get; set; }
         public string ServerName { get; set; }
         public Guid JobID { get; set; }
         public Guid ScheduleUID { get; set; }
 
         // OneTime Schedules
+        [Required]
+        [Display(Name = "Start date")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime OneTimeStartDate { get; set; }
+        [Required]
+        [Display(Name = "Start time")]
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan OneTimeStartTimeOfDay { get; set; }
 
 
         // Daily Schedules
-        [Range(typeof(int), "1", "100")]
+        [Required]
+        [Display(Name = "Daily frequency")]
+        [Range(typeof(int), "1", "100", ErrorMessage = "Number must be between 1 and 100.")]
         public int DailyRecursEvery { get; set; }
 
 
         // Weekly Schedules
-        [Range(typeof(int), "1", "100")]
+        [Required]
+        [Display(Name = "Weekly frequency")]
+        [Range(typeof(int), "1", "100", ErrorMessage = "Number must be between 1 and 100.")]
         public int WeeklyRecursEvery { get; set; }
         public bool WeeklyMonday { get; set; }
         public bool WeeklyTuesday { get; set; }
@@ -46,40 +57,66 @@ namespace JobManager.Models
 
 
         // Monthly Schedules
-        [Range(typeof(int), "1", "31")]
+        [Required]
+        [Display(Name = "Monthly day number")]
+        [Range(typeof(int), "1", "31", ErrorMessage = "Number must be between 1 and 31.")]
         public int MonthlyDayNo { get; set; }
-        [Range(typeof(int), "1", "99")]
+
+        [Required]
+        [Display(Name = "Monthly frequency")]
+        [Range(typeof(int), "1", "99", ErrorMessage = "Number must be between 1 and 99.")]
         public int MonthlyFrequency { get; set; }
 
 
         // Monthly Relative Schedules
-        [Range(typeof(int), "1", "99")]
+        [Required]
+        [Display(Name = "Monthly frequency")]
+        [Range(typeof(int), "1", "99", ErrorMessage = "Number must be between 1 and 99.")]
         public int MonthlyRelativeFreq { get; set; }
+
         public string MonthlyRelativeFreqSubDayType { get; set; }
+
         public string MonthlyRelativeSubFreq { get; set; }
 
 
 
         // Daily Frequency for Daily, Weekly and Monthly
         public bool DailyFreqOccursOnce { get; set; }
+
+        [Required]
+        [Display(Name = "Daily start time")]
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan DailyFreqOccursOnceTime { get; set; }
-        [Range(typeof(int), "1", "100")]
+
+        [Required]
+        [Display(Name = "Daily occurrance")]
+        [Range(typeof(int), "1", "100", ErrorMessage = "Number must be between 1 and 100.")]
         public int DailyFreqOccursEvery { get; set; }
         public string DailyFreqSubDay { get; set; }
+
+        [Required]
+        [Display(Name = "Daily start time")]
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan DailyFreqStartingTime { get; set; }
+
+        [Required]
+        [Display(Name = "Daily end time")]
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan DailyFreqEndingTime { get; set; }
 
 
         // duration for Daily, Weekly and Monthly
+        [Required]
+        [Display(Name = "Schedule start date")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime DurationStartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Schedule end date")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime DurationEndDate { get; set; }
@@ -87,20 +124,15 @@ namespace JobManager.Models
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            switch (ScheduleFrequency)
+            if (ScheduleFrequency == "OneTime" && OneTimeStartDate.Date < DateTime.Now.Date)
             {
-                case "OneTime":
-                    if (OneTimeStartDate.Date < DateTime.Now.Date)
-                    {
-                        yield return new ValidationResult("Date invalid");
-                    }
-                    break;
-                case "Daily":
-                    if (DailyRecursEvery < 1)
-                    {
-                        yield return new ValidationResult("Daily frequency invalid");
-                    }
-                    break;
+                yield return new ValidationResult("Start Date cannot be before today.");
+            }
+
+            if (ScheduleFrequency == "Weekly" 
+                && (!WeeklySunday || !WeeklyMonday || !WeeklyTuesday || !WeeklyWednesday || !WeeklyThursday || !WeeklyFriday || !WeeklySaturday))
+            {
+                yield return new ValidationResult("At least one day must be selected.");
             }
         }
     }
