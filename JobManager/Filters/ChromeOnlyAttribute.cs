c59bc3bb89ae818c2c95835fccdc60082ee2d16a
crimdon <crimdon@hotmail.com>
2017-01-10 14:56:47 +0000

    checks browser and group memebership

---
+++ JobManager/Filters/ChromeOnlyAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobManager.Filters
+{
+    public class ChromeOnlyAttribute : ActionFilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var userAgent = filterContext.HttpContext.Request.Headers["User-Agent"];
+            if (!IsChrome(userAgent))
+            {
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "~/Views/Shared/Unauthorized.cshtml"
+                };
+            }
+        }
+
+        private bool IsChrome(string userAgent)
+        {
+            if (userAgent.Contains("Chrome"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
